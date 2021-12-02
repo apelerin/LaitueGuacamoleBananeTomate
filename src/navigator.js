df6cf9a5a999b4bd74ff89a526266a1d8f9f3c26
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2021-12-02 22:32:30 +0100

    page Details front and back dosent work

--- src/navigator.js
+++ src/navigator.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import RecipeListScreen from './screens/recipeListScreen';
+import DetailScreen from './screens/detailscreendir/detailScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="recipeList"
+      initialRouteName="DetailScreen"
       screenOptions={{
         headerShown: false,
       }}>
       <Stack.Screen name="recipeList" component={RecipeListScreen} />
+      <Stack.Screen name="DetailScreen" component={DetailScreen} />
     </Stack.Navigator>
   );
 };
--- src/screens/detailscreendir/detailsstyles.js
+++ src/screens/detailscreendir/detailsstyles.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import {
-    FlatList,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Text,
-    StyleSheet,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
 } from 'react-native';
 
-export default  StyleSheet.create({
-    main:{
-        backgroundColor: '#FEE7F0',
-    }
-    }
-)
-
+export default StyleSheet.create({
+  main: {
+    backgroundColor: '#E2FCEB',
+    flex: 3,
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+  text: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    color: '#048B9A',
+  },
+  Image2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  ingredient: {
+    flex: 1,
+  },
+});
