68b83d78b6585990dfd920c9939b41f3689d0f32
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2021-12-02 22:23:31 +0100

    page Details front and back dosent work

---
+++ src/screens/detailscreendir/detailsstyles.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    FlatList,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+
+export default  StyleSheet.create({
+    main:{
+        backgroundColor: '#FEE7F0',
+    }
+    }
+)
+
