8e6a69efc8924aa1a9c7028902659ebb166f9d0e
Clément <clement@anis.local>
2021-12-02 10:39:02 +0100

    wip axios

--- App.js
+++ App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigator';
 
+import axios from 'axios';
+const baseUrl = 'https://www.themealdb.com/api/json';
+
 const App = () => {
+  // Invoking get method to perform a GET request
+  axios.get(`${baseUrl}/v1/1/search.php?s=Arrabiata`).then(response => {
+    console.log(response.data);
+  });
+
   return (
     <NavigationContainer>
       <AppNavigator />
